88abbb1be9f60e7db87da9b708818baa396f2cb3
darrkenn <darrkenn08@gmail.com>
2025-12-06 22:32:03 +0000

    Basic sentence generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{array, fs};
-
+mod words;
 use genetica::{
     crossover::single_point_crossover,
     individual::{Generate, Individual, Mutate},
@@ -7,54 +7,147 @@ use genetica::{
 };
 use serde::Deserialize;
 
+use crate::words::{
+    ADJECTIVES, ADJECTIVES_COUNT, ADVERBS, ADVERBS_COUNT, CONJUNCTIONS, CONJUNCTIONS_COUNT,
+    DETERMINERS, DETERMINERS_COUNT, NOUNS, NOUNS_COUNT, PREPOSITIONS, PREPOSITIONS_COUNT, VERBS,
+    VERBS_COUNT,
+};
+
 const PC: f32 = 0.6;
 const PM: f32 = 0.05;
 
-const ARRAYSIZE: usize = 17;
-//There has to be a better way to do this
-const SENTENCE: [char; ARRAYSIZE] = [
-    'h', 'e', 'y', ' ', 'i', 'm', ' ', 'a', ' ', 'c', 'o', 'm', 'p', 'u', 't', 'e', 'r',
-];
+const WORD_COUNT: usize = 6;
 
-const AVAILABLE_CHARS_COUNT: usize = 27;
-const AVAILABLE_CHARS: [char; AVAILABLE_CHARS_COUNT] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', ' ',
-];
-/*
-const VALUES: [i32; ARRAYSIZE] = [75, 22, 91, 48, 15, 63, 30, 88, 55, 12, 99, 40];
-const WEIGHTS: [i32; ARRAYSIZE] = [3, 1, 5, 2, 1, 4, 2, 5, 3, 1, 5, 2];
-const MAX_WEIGHT: i32 = 12;
-*/
+const NOUN_RATE: f32 = 0.30;
+const VERB_RATE: f32 = 0.20;
+const ADVERB_RATE: f32 = 0.10;
+const ADJECTIVE_RATE: f32 = 0.10;
+const PREPOSITION_RATE: f32 = 0.10;
+const DETERMINER_RATE: f32 = 0.10;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-struct GeneType(pub char);
+enum WordType {
+    Noun,
+    Verb,
+    Adverb,
+    Adjective,
+    Conjunction,
+    Preposition,
+    Determiner,
+}
 
-impl Generate for GeneType {
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct GeneType<'a>(pub &'a str, pub WordType);
+
+impl<'a> Generate for GeneType<'a> {
     fn generate() -> Self {
-        GeneType(AVAILABLE_CHARS[rand::random_range(0..AVAILABLE_CHARS_COUNT)])
+        let random_f32: f32 = rand::random_range(0.00..1.00);
+        //This seems stupid
+        let (word, word_type) = if random_f32 <= NOUN_RATE {
+            (NOUNS[rand::random_range(0..NOUNS_COUNT)], WordType::Noun)
+        } else if random_f32 <= (NOUN_RATE + VERB_RATE) {
+            (VERBS[rand::random_range(0..VERBS_COUNT)], WordType::Verb)
+        } else if random_f32 <= (NOUN_RATE + VERB_RATE + ADVERB_RATE) {
+            (
+                ADVERBS[rand::random_range(0..ADVERBS_COUNT)],
+                WordType::Adverb,
+            )
+        } else if random_f32 <= (NOUN_RATE + VERB_RATE + ADVERB_RATE + ADJECTIVE_RATE) {
+            (
+                ADJECTIVES[rand::random_range(0..ADJECTIVES_COUNT)],
+                WordType::Adjective,
+            )
+        } else if random_f32
+            <= (NOUN_RATE + VERB_RATE + ADVERB_RATE + ADJECTIVE_RATE + PREPOSITION_RATE)
+        {
+            (
+                PREPOSITIONS[rand::random_range(0..PREPOSITIONS_COUNT)],
+                WordType::Preposition,
+            )
+        } else if random_f32
+            <= (NOUN_RATE
+                + VERB_RATE
+                + ADVERB_RATE
+                + ADJECTIVE_RATE
+                + PREPOSITION_RATE
+                + DETERMINER_RATE)
+        {
+            (
+                DETERMINERS[rand::random_range(0..DETERMINERS_COUNT)],
+                WordType::Determiner,
+            )
+        } else {
+            (
+                CONJUNCTIONS[rand::random_range(0..CONJUNCTIONS_COUNT)],
+                WordType::Conjunction,
+            )
+        };
+        GeneType(word, word_type)
     }
 }
 
-impl Mutate for GeneType {
+impl<'a> Mutate for GeneType<'a> {
     fn mutate(&mut self) {
         if rand::random_range(0.00..1.00) <= PM {
-            self.0 = AVAILABLE_CHARS[rand::random_range(0..AVAILABLE_CHARS_COUNT)]
+            let random_f32: f32 = rand::random_range(0.00..1.00);
+
+            let (word, word_type) = if random_f32 <= NOUN_RATE {
+                (NOUNS[rand::random_range(0..NOUNS_COUNT)], WordType::Noun)
+            } else if random_f32 <= (NOUN_RATE + VERB_RATE) {
+                (VERBS[rand::random_range(0..VERBS_COUNT)], WordType::Verb)
+            } else if random_f32 <= (NOUN_RATE + VERB_RATE + ADVERB_RATE) {
+                (
+                    ADVERBS[rand::random_range(0..ADVERBS_COUNT)],
+                    WordType::Adverb,
+                )
+            } else if random_f32 <= (NOUN_RATE + VERB_RATE + ADVERB_RATE + ADJECTIVE_RATE) {
+                (
+                    ADJECTIVES[rand::random_range(0..ADJECTIVES_COUNT)],
+                    WordType::Adjective,
+                )
+            } else if random_f32
+                <= (NOUN_RATE + VERB_RATE + ADVERB_RATE + ADJECTIVE_RATE + PREPOSITION_RATE)
+            {
+                (
+                    PREPOSITIONS[rand::random_range(0..PREPOSITIONS_COUNT)],
+                    WordType::Preposition,
+                )
+            } else if random_f32
+                <= (NOUN_RATE
+                    + VERB_RATE
+                    + ADVERB_RATE
+                    + ADJECTIVE_RATE
+                    + PREPOSITION_RATE
+                    + DETERMINER_RATE)
+            {
+                (
+                    DETERMINERS[rand::random_range(0..DETERMINERS_COUNT)],
+                    WordType::Determiner,
+                )
+            } else {
+                (
+                    CONJUNCTIONS[rand::random_range(0..CONJUNCTIONS_COUNT)],
+                    WordType::Conjunction,
+                )
+            };
+
+            self.0 = word;
+            self.1 = word_type;
         };
     }
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Chromosome {
-    genes: [GeneType; ARRAYSIZE],
-    fitness: Option<i32>,
+struct Chromosome<'a> {
+    genes: [GeneType<'a>; WORD_COUNT],
+    fitness: Option<f32>,
 }
 
-impl Individual for Chromosome {
-    type GeneType = GeneType;
-    const GENES_SIZE: usize = ARRAYSIZE;
+impl<'a> Individual for Chromosome<'a> {
+    type GeneType = GeneType<'a>;
+    const GENES_SIZE: usize = WORD_COUNT;
     fn new() -> Self {
-        let genes: [GeneType; ARRAYSIZE] = array::from_fn(|_| GeneType::generate());
+        let genes: [GeneType; WORD_COUNT] = array::from_fn(|_| GeneType::generate());
         Chromosome {
             genes,
             fitness: None,
@@ -73,20 +166,94 @@ impl Individual for Chromosome {
         &mut self.genes
     }
 
-    fn fitness(&self) -> Option<i32> {
+    fn fitness(&self) -> Option<f32> {
         self.fitness
     }
-    fn fitness_mut(&mut self) -> &mut Option<i32> {
+    fn fitness_mut(&mut self) -> &mut Option<f32> {
         &mut self.fitness
     }
 
     fn calculate_fitness(&mut self) {
-        let fitness = SENTENCE
-            .iter()
-            .zip(self.genes)
-            .filter(|(cw, cg)| **cw == cg.0)
-            .count();
-        self.fitness = Some(fitness as i32);
+        let mut fitness: f32 = 0.0;
+        let mut previous_word: Option<&WordType> = None;
+        let last_word = self.genes.last();
+
+        self.genes.iter().for_each(|gt| {
+            let word_type = &gt.1;
+            if let Some(previous_word) = previous_word {
+                match word_type {
+                    WordType::Noun => match previous_word {
+                        WordType::Noun => {
+                            fitness -= 0.2;
+                        }
+
+                        WordType::Adjective => {
+                            fitness += 0.2;
+                        }
+                        WordType::Conjunction => {
+                            fitness -= 0.05;
+                        }
+                        WordType::Adverb => {
+                            fitness -= 0.3;
+                        }
+                        WordType::Verb => {
+                            fitness -= 0.1;
+                        }
+                        WordType::Preposition | WordType::Determiner => fitness += 0.3,
+                    },
+                    WordType::Verb => match previous_word {
+                        WordType::Noun => {
+                            if gt.0.ends_with("ing") {
+                                fitness -= 0.1
+                            } else if gt.0.ends_with("ed") || gt.0.ends_with("pt") {
+                                fitness += 0.2
+                            } else {
+                                fitness += 0.1
+                            }
+                        }
+                        WordType::Adjective => fitness -= 0.1,
+                        _ => fitness -= 0.05,
+                    },
+                    WordType::Adverb => match previous_word {
+                        WordType::Verb => {
+                            fitness += 0.2;
+                        }
+                        WordType::Adverb => {
+                            fitness -= 0.1;
+                        }
+                        _ => fitness -= 0.1,
+                    },
+                    WordType::Adjective => match previous_word {
+                        WordType::Noun => fitness -= 0.2,
+                        _ => fitness += 0.1,
+                    },
+                    WordType::Conjunction => match previous_word {
+                        WordType::Noun => fitness += 0.1,
+                        WordType::Conjunction => fitness -= 0.3,
+                        _ => fitness -= 0.1,
+                    },
+                    WordType::Preposition => {}
+                    WordType::Determiner => {}
+                }
+            } else {
+                if word_type == &WordType::Noun {
+                    fitness += 0.5;
+                } else if word_type == &WordType::Adjective {
+                    fitness += 0.3;
+                } else {
+                    fitness -= 0.1;
+                }
+            }
+            previous_word = Some(word_type)
+        });
+        if let Some(last_word) = last_word {
+            match last_word.1 {
+                WordType::Conjunction => fitness -= 0.3,
+                WordType::Adverb => fitness -= 0.1,
+                _ => {}
+            }
+        };
+        self.fitness = Some(fitness)
     }
 }
 
@@ -127,9 +294,14 @@ fn main() {
 
     sort_population_descending(&mut population);
     let best = &population[0];
-    let best_constructed_word: String = best.genes.iter().map(|g| g.0).collect();
+    let best_constructed_word = best
+        .genes
+        .iter()
+        .map(|g| g.0.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
     println!(
-        "Fitness: {}\nWord: {}",
+        "Fitness: {}\nWord: {}.",
         best.fitness.unwrap(),
         best_constructed_word
     );
---
+++ src/words.rs
@@ -0,0 +1,72 @@
+pub const NOUNS_COUNT: usize = 19;
+pub const NOUNS: [&str; NOUNS_COUNT] = [
+    "cat",
+    "sprinkle",
+    "france",
+    "cheese",
+    "tree",
+    "river",
+    "computer",
+    "idea",
+    "fish",
+    "rust",
+    "genetic",
+    "algorithm",
+    "apple",
+    "dog",
+    "chocolate",
+    "joe",
+    "pat",
+    "sheamus",
+    "franklin",
+];
+
+pub const VERBS_COUNT: usize = 8;
+pub const VERBS: [&str; VERBS_COUNT] = [
+    "jumped",
+    "walked",
+    "talking",
+    "attacked",
+    "ran",
+    "slept",
+    "programmed",
+    "imagined",
+];
+
+pub const ADVERBS_COUNT: usize = 8;
+pub const ADVERBS: [&str; ADVERBS_COUNT] = [
+    "quickly",
+    "excitedly",
+    "slowly",
+    "happily",
+    "loudly",
+    "softly",
+    "carefully",
+    "eagerly",
+];
+pub const ADJECTIVES_COUNT: usize = 8;
+pub const ADJECTIVES: [&str; ADJECTIVES_COUNT] = [
+    "silly",
+    "courageous",
+    "tasty",
+    "short",
+    "bright",
+    "complex",
+    "smooth",
+    "ancient",
+];
+
+pub const CONJUNCTIONS_COUNT: usize = 11;
+pub const CONJUNCTIONS: [&str; CONJUNCTIONS_COUNT] = [
+    "and", "then", "after", "because", "but", "so", "while", "though", "for", "or", "yet",
+];
+
+pub const DETERMINERS_COUNT: usize = 10;
+pub const DETERMINERS: [&str; DETERMINERS_COUNT] = [
+    "the", "a", "an", "this", "that", "these", "those", "some", "many", "few",
+];
+
+pub const PREPOSITIONS_COUNT: usize = 8;
+pub const PREPOSITIONS: [&str; PREPOSITIONS_COUNT] = [
+    "in", "on", "at", "under", "behind", "before", "after", "near",
+];
